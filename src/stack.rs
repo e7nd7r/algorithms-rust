7ec7b7c25e93ad7d07e89b3828f6d4fdf9704766
M. Esteban Perez Dorador <esteban5003@gmail.com>
2023-11-24 19:15:21 -0800

    daily temperatures

--- src/stack.rs
+++ src/stack.rs
@@ -2,3 +2,4 @@ pub mod valid_parenthesis;
 pub mod min_stack;
 pub mod eval_polish_notation;
 pub mod generate_parenthesis;
+pub mod daily_temperatures;
---
+++ src/stack/daily_temperatures.rs
@@ -0,0 +1,44 @@
+struct Solution {}
+
+impl Solution {
+    pub fn daily_temperatures(temperatures: Vec<i32>) -> Vec<i32> {
+        let mut stack : Vec<usize> = Vec::with_capacity(temperatures.len());
+        let mut res : Vec<i32> = vec![0; temperatures.len()];
+
+        for (index,temp) in temperatures.iter().enumerate() {
+                while let Some(last_temp_index) = stack.last(){
+                    if *temp > temperatures[*last_temp_index] {
+                        res[*last_temp_index] = (index - *last_temp_index) as i32;
+                        stack.pop();
+                    } else {
+                        break;
+                    }
+                }
+
+                stack.push(index);
+        }
+
+        res
+    }
+
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::stack::daily_temperatures::Solution;
+
+    #[test]
+    fn test_dayly_temperatures_01() {
+        assert_eq!(Solution::daily_temperatures(vec![73,74,75,71,69,72,76,73]), vec![1,1,4,2,1,1,0,0]);
+    }
+
+    #[test]
+    fn test_dayly_temperatures_02() {
+        assert_eq!(Solution::daily_temperatures(vec![30,40,50,60]), vec![1,1,1,0]);
+    }
+
+    #[test]
+    fn test_dayly_temperatures_03() {
+        assert_eq!(Solution::daily_temperatures(vec![30,60,90]), vec![1,1,0]);
+    }
+}
